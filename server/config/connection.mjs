6437645abe8f4109e913b86334f412518a78518a
Kevin Lewis <20131699+KPL33@users.noreply.github.com>
2024-07-19 16:21:41 -0700

    simplified approach to connection file

--- server/config/connection.mjs
+++ server/config/connection.mjs
@@ -1,42 +1,21 @@
-import path from "path";
-import { config } from "dotenv";
 import { Sequelize } from "sequelize";
-import { fileURLToPath } from "url";
-import { dirname } from "path";
-
-// Get directory name
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import { config } from "dotenv";
 
 // Load environment variables from .env file
-config({ path: path.resolve(__dirname, "../../.env") });
+config();
 
-// Extract environment variables
-const {
-  DB_NAME = "default_db_name",
-  DB_USER = "default_user",
-  DB_PASSWORD = "default_password",
-  DB_HOST = "localhost",
-  DB_PORT = 3306,
-  JAWSDB_URL,
-} = process.env;
-
-// Create Sequelize instance
-const sequelize =
-  process.env.NODE_ENV === "production" && JAWSDB_URL
-    ? new Sequelize(JAWSDB_URL, {
+// Create Sequelize instance based on environment
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        host: "localhost",
         dialect: "mysql",
-        dialectOptions: {
-          ssl: {
-            require: true,
-            rejectUnauthorized: false,
-          },
-        },
-      })
-    : new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
-        host: DB_HOST,
-        port: parseInt(DB_PORT, 10),
-        dialect: "mysql", // Hardcode 'mysql' for clarity
-      });
+        port: parseInt(process.env.DB_PORT, 10) || 3306, // Use environment variable or default to 3306
+      }
+    );
 
 export default sequelize;
